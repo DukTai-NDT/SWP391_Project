425db95fa87157c4cd53184abf25a73109e57799
DukTai-NDT <nguyenductai.12a2tqk@gmail.com>
2025-02-07 17:27:45 +0700

    update project

---
+++ src/java/entity/Doctor.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author Admin
+ */
+public class Doctor {
+    private int DoctorID;
+	private String FirstName;
+	private String LastName;
+	private String Phone;
+	Email;
+	[Gender;
+	[DepartmentID;
+	[Bio;
+	[RoleID;
+}
